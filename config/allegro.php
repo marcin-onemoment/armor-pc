e25ec1b36947fd7d0926f93d3ea5fed0b7dd0521
Marcin Michalak <marcin.michalak.onemoment@gmail.com>
2023-05-18 21:00:36 +0200

    config

--- config/allegro.php
+++ config/allegro.php
@@ -3,7 +3,7 @@
 return [
     'client_id' => env('ALLEGRO_CLIENT_ID', 'e8db55f35ad94098be80d829f412c7b2'),
     'client_secret' => env('ALLEGRO_CLIENT_SECRET', 'AzXcpto1GfcrKWDMfPHGH3H1EJzKeO5vPKdJjsCwzynQGPwWILA6LHa8yqs1YIhF'),
-    'redirect_url' => env('ALLEGRO_REDIRECT_URL', 'http://8da1-217-173-4-61.ngrok-free.app/allegro/save-authorization'),
+    'redirect_url' => env('ALLEGRO_REDIRECT_URL', 'https://armorpc.pl/allegro/save-authorization'),
     'is_sandbox' => env('ALLEGRO_IS_SANDBOX', false),
     'offer_link' => env('ALLEGRO_OFFER_LINK', 'https://allegro.pl/oferta/'),
     'shop_link' => env('ALLEGRO_SHOP_LINK', 'https://allegro.pl/uzytkownik/ArmorPC')
