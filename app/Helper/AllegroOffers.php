18be45640a0323e51eda125e9ea35e03a5812a60
Marcin Michalak <marcin.michalak.onemoment@gmail.com>
2023-05-19 09:04:30 +0200

    change categroies

--- app/Helper/AllegroOffers.php
+++ app/Helper/AllegroOffers.php
@@ -13,7 +13,7 @@ class AllegroOffers
 
         $categories = AllegroProduct::select('category_id', DB::raw('count(*) as total'))
             ->orderBy('total', 'DESC')
-            ->limit(6)
+            ->limit(5)
             ->groupBy('category_id')
             ->pluck('total','category_id')
             ->toArray();
